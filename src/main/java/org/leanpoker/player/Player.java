65a7c70610220e60802cbf6c50e8548b675b8938
Akos Kolloros <kollorosa@gmail.com>
2019-01-14 10:16:37 +0100

    deployTest

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +10,10 @@ public class Player {
     static final String VERSION = "0.1";
 
     public static int betRequest(JsonElement request) {
+        JsonObject object = request.getAsJsonObject();
+        System.err.println(object.toString());
         return 0;
+
     }
 
     public static void showdown(JsonElement game) {
