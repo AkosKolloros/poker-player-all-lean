9b313a7cdfdfe47fd83cceff25f2f4e916401dc0
Andras <horinka.andras@gmail.com>
2019-01-14 15:51:11 +0100

    merge

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "0.13";
+    static final String VERSION = "0.14";
 
     public static int betRequest(JsonElement request) {
         try{
