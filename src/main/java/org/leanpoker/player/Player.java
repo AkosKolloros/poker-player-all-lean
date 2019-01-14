9fb1760d811b8e1387e3abf717473304320d4fbd
Adam Gonda <adamgondagyula@gmail.com>
2019-01-14 15:58:38 +0100

    refactored

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonElement;
 public class Player {
 
     static final String VERSION = "0.12";
-    static final PlayLogic playerLogic = null;
 
     public static int betRequest(JsonElement request) {
         Logic playerLogic = new CurrentLogic(request);
