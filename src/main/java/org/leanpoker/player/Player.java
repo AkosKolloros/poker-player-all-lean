98c405df0af72c878d18662757c16c3455f8da87
Akos Kolloros <kollorosa@gmail.com>
2019-01-14 14:28:49 +0100

    Card inputs

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,12 +8,14 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "0.8";
+    static final String VERSION = "0.9";
 
     public static int betRequest(JsonElement request) {
         try{
             JsonObject gameInfo = request.getAsJsonObject();
 
+            int round = gameInfo.get("round").getAsInt();
+            int pot = gameInfo.get("pot").getAsInt();
 
             JsonArray players = gameInfo.getAsJsonArray("players");
 
@@ -22,6 +24,30 @@ public class Player {
 
             JsonArray hole_cards = player.getAsJsonArray("hole_cards");
 
+            Card firstHand = new Card(hole_cards.get(0).getAsJsonArray().get(1).getAsString(), hole_cards.get(0).getAsJsonArray().get(0).getAsString());
+            Card secondHand = new Card(hole_cards.get(1).getAsJsonArray().get(1).getAsString(), hole_cards.get(1).getAsJsonArray().get(0).getAsString());
+
+            JsonArray communityCards = gameInfo.getAsJsonArray("community_cards");
+            if (communityCards.size() == 3){
+                Card firstCommunity = new Card(communityCards.get(0).getAsJsonArray().get(1).getAsString(), communityCards.get(0).getAsJsonArray().get(0).getAsString());
+                Card secondCommunity = new Card(communityCards.get(1).getAsJsonArray().get(1).getAsString(), communityCards.get(1).getAsJsonArray().get(0).getAsString());
+                Card thirdCommunity = new Card(communityCards.get(2).getAsJsonArray().get(1).getAsString(), communityCards.get(2).getAsJsonArray().get(0).getAsString());
+            }
+            else if (communityCards.size() == 4){
+                Card firstCommunity = new Card(communityCards.get(0).getAsJsonArray().get(1).getAsString(), communityCards.get(0).getAsJsonArray().get(0).getAsString());
+                Card secondCommunity = new Card(communityCards.get(1).getAsJsonArray().get(1).getAsString(), communityCards.get(1).getAsJsonArray().get(0).getAsString());
+                Card thirdCommunity = new Card(communityCards.get(2).getAsJsonArray().get(1).getAsString(), communityCards.get(2).getAsJsonArray().get(0).getAsString());
+                Card fourthCommunity = new Card(communityCards.get(3).getAsJsonArray().get(1).getAsString(), communityCards.get(3).getAsJsonArray().get(0).getAsString());
+
+            }
+            else if (communityCards.size() == 5){
+                Card firstCommunity = new Card(communityCards.get(0).getAsJsonArray().get(1).getAsString(), communityCards.get(0).getAsJsonArray().get(0).getAsString());
+                Card secondCommunity = new Card(communityCards.get(1).getAsJsonArray().get(1).getAsString(), communityCards.get(1).getAsJsonArray().get(0).getAsString());
+                Card thirdCommunity = new Card(communityCards.get(2).getAsJsonArray().get(1).getAsString(), communityCards.get(2).getAsJsonArray().get(0).getAsString());
+                Card fourthCommunity = new Card(communityCards.get(3).getAsJsonArray().get(1).getAsString(), communityCards.get(3).getAsJsonArray().get(0).getAsString());
+                Card fifthCommunity = new Card(communityCards.get(4).getAsJsonArray().get(1).getAsString(), communityCards.get(4).getAsJsonArray().get(0).getAsString());
+
+            }
 
             System.err.println("ALL LEAN");
             System.err.println(hole_cards.get(0));
